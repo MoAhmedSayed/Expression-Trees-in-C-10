83a59e1fb595f01149c0f39990de8e62821c4947
Mohamed Ahmed Sayed <sayed@goodlawsoftware.co.uk>
2022-08-13 23:56:19 +0100

    Delegate Refresher

---
+++ Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Delegates
+{
+   internal class Program
+   {
+      public delegate void WriteMessageFunction(string message);
+      public static void WriteMessage(string message)
+      {
+         Console.WriteLine(message);
+      }
+      public static void WriteTimlyMessage(string message)
+      {
+         Console.WriteLine(message + " " + DateTime.Now);
+      }
+      private static void Main(string[] args)
+      {
+         var selection = Console.ReadLine();
+         WriteMessageFunction writeMessageFunction;
+         if (selection == "1")
+         {
+            writeMessageFunction = WriteTimlyMessage;
+         }
+         else
+         {
+            writeMessageFunction = WriteMessage;
+         }
+         writeMessageFunction("Hello, World!");
+      }
+   }
+}
